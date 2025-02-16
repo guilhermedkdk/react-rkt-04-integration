83485e8e4cb9afabe7e33acab981934823f93ac3
guilhermedkdk <guilhermeperesmm@gmail.com>
2025-02-16 16:12:43 -0300

    chore: set up API config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './index.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { Toaster } from '@/components/ui/sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
@@ -15,7 +17,9 @@ export function App() {
         <Helmet titleTemplate="%s | pizza.shop" />
 
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
